aeb4befcf0e8eb84c983340c5bb636e98d1f7f3c
Alejandro Torras <atec.post@gmail.com>
2017-01-17 13:00:41 +0100

    NEW 1 bit NotLogic

---
+++ predictor/src/com/github/atorr0/predictor/logic/_1bit/NotLogic.java
@@ -0,0 +1,47 @@
+package com.github.atorr0.predictor.logic._1bit;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Simple prediction logic that negates the underlying Logic.
+ *
+ * @author https://github.com/atorr0
+ */
+public class NotLogic extends _1BitStandaloneLogic {
+
+	protected _1BitStandaloneLogic delegateLogic;
+
+	public NotLogic(_1BitStandaloneLogic logic) {
+		delegateLogic = logic;
+	}
+
+	public Iterator<Boolean> iterator() {
+		return delegateLogic.iterator();
+	}
+
+	@Override
+	public void feedback(Boolean t) {
+		delegateLogic.feedback(t);
+	}
+
+	@Override
+	public Boolean predict(List<Boolean> previousResults) {
+		return delegateLogic.predict(previousResults);
+	}
+
+	public void build(byte[] bs) {
+		delegateLogic.build(bs);
+	}
+
+	public void build(InputStream is) throws IOException {
+		delegateLogic.build(is);
+	}
+
+	public void run() {
+		delegateLogic.run();
+	}
+
+}
